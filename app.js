70fd1a956717ab56fb3a2c6a37bc79ca3629b9b1
jrodr117 <jrodr113@msudenver.edu>
2022-06-07 13:44:40 -0600

    installed ejs and used a switch to render the day on the ejs file

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	var today = new Date();
+	var currentDay = today.getDay();
+	var day = '';
+
+	// if (currentDay === 6 || currentDay === 0) {
+	// 	day = 'Weekend';
+	// } else if {
+	// 	day = 'Weekday';
+	// }
+
+	switch (currentDay) {
+		case 0:
+			day = 'Sunday';
+			break;
+		case 1:
+			day = 'Monday';
+			break;
+		case 2:
+			day = 'Tuesday';
+			break;
+		case 3:
+			day = 'Wednesday';
+			break;
+		case 4:
+			day = 'Thursday';
+			break;
+		case 5:
+			day = 'Friday';
+			break;
+		case 6:
+			day = 'Saturday';
+	}
+
+	res.render('list', { kindOfDay: day });
+});
+
+app.listen(3000, () => {
+	console.log('run server, run');
+});
