9be693cce8e38a8369466c825e7733b4644f9877
jrodr117 <jrodr113@msudenver.edu>
2022-06-07 15:09:01 -0600

    refactored to do list html code and added logic to add individual items to the to do list

--- app.js
+++ app.js
@@ -4,42 +4,22 @@ const ejs = require('ejs');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var items = ['groceries', 'cook', 'eat'];
 
 app.get('/', (req, res) => {
 	var today = new Date();
-	var currentDay = today.getDay();
-	var day = '';
+	var options = { weekday: 'long', day: 'numeric', month: 'long' };
+	var day = today.toLocaleDateString('en-us', options);
 
-	// if (currentDay === 6 || currentDay === 0) {
-	// 	day = 'Weekend';
-	// } else if {
-	// 	day = 'Weekday';
-	// }
+	res.render('list', { kindOfDay: day, newListItems: items });
+});
 
-	switch (currentDay) {
-		case 0:
-			day = 'Sunday';
-			break;
-		case 1:
-			day = 'Monday';
-			break;
-		case 2:
-			day = 'Tuesday';
-			break;
-		case 3:
-			day = 'Wednesday';
-			break;
-		case 4:
-			day = 'Thursday';
-			break;
-		case 5:
-			day = 'Friday';
-			break;
-		case 6:
-			day = 'Saturday';
-	}
-
-	res.render('list', { kindOfDay: day });
+app.post('/', (req, res) => {
+	var item = req.body.listItem;
+	items.push(item);
+	res.redirect('/');
 });
 
 app.listen(3000, () => {
