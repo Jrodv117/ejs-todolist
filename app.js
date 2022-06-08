1802b5d88b620da91b32d100bc24091ffa10ea60
jrodr117 <jrodr113@msudenver.edu>
2022-06-07 19:02:51 -0600

    Added a work to do list route with full functionality

--- app.js
+++ app.js
@@ -8,19 +8,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 let items = ['groceries', 'cook', 'eat'];
+let workItems = [];
 
 app.get('/', (req, res) => {
 	let today = new Date();
 	let options = { weekday: 'long', day: 'numeric', month: 'long' };
 	let day = today.toLocaleDateString('en-us', options);
 
-	res.render('list', { kindOfDay: day, newListItems: items });
+	res.render('list', { listTitle: day, newListItems: items });
 });
 
 app.post('/', (req, res) => {
 	let item = req.body.listItem;
-	items.push(item);
-	res.redirect('/');
+	if (req.body.list === 'Work') {
+		workItems.push(item);
+		res.redirect('/work');
+	} else {
+		items.push(item);
+		res.redirect('/');
+	}
+});
+
+app.get('/work', (req, res) => {
+	res.render('list', { listTitle: 'Work To-Do List', newListItems: workItems });
 });
 
 app.listen(3000, () => {
