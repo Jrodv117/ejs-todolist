99d8711184b319cd7be661740d9ca6e6069868a4
jrodr117 <jrodr113@msudenver.edu>
2022-06-07 18:32:10 -0600

    added some styling to my to do list

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const ejs = require('ejs');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-var items = ['groceries', 'cook', 'eat'];
+let items = ['groceries', 'cook', 'eat'];
 
 app.get('/', (req, res) => {
-	var today = new Date();
-	var options = { weekday: 'long', day: 'numeric', month: 'long' };
-	var day = today.toLocaleDateString('en-us', options);
+	let today = new Date();
+	let options = { weekday: 'long', day: 'numeric', month: 'long' };
+	let day = today.toLocaleDateString('en-us', options);
 
 	res.render('list', { kindOfDay: day, newListItems: items });
 });
 
 app.post('/', (req, res) => {
-	var item = req.body.listItem;
+	let item = req.body.listItem;
 	items.push(item);
 	res.redirect('/');
 });
